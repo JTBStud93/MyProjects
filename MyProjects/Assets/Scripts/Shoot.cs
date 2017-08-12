4e0af3bcebdf54933733f6b6fd3a8ff42567eb0e
J. Tanner Breeze <JTBStud@gmail.com>
2017-08-12 14:42:35 -0600

    Player Bullet

---
+++ MyProjects/Assets/Scripts/Shoot.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+	
+	public Rigidbody projectile;
+	public Transform Spawnpoint;
+	public int shootSpeed;
+	
+	void Update () {
+		if(Input.GetButtonDown("Fire1")){
+			Rigidbody clone;
+			clone = (Rigidbody)Instantiate(projectile, Spawnpoint.position, projectile.rotation);
+			clone.velocity = Spawnpoint.TransformDirection (Vector3.forward*shootSpeed);
+		}
+	}
+}
